ac574d5d83a8d3bea8d322579b9c7b446a3f67ce
papilon <ademirgus@gmail.com>
2023-06-05 15:37:00 -0300

    feat:finalizing component styling

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,3 +1,10 @@
 export function Chat() {
-  return <h1>Chat</h1>;
+  return (
+    // Final 30 min
+    <div className="flex-1 shadow-[-8px_10px_10px_-5px_rgba(0,0,0,0.1)] overflow-hidden">
+      <div className="flex flex-col w-full h-full overflow-hidden">
+        <div className="flex w-full h-[60px] items-center justify-between px-5 border-b border-neutral-200"></div>
+      </div>
+    </div>
+  );
 }
--- src/components/Chatlist.tsx
+++ src/components/Chatlist.tsx
@@ -1,3 +1,27 @@
+import { FunnelSimple, Gear } from "phosphor-react";
+
 export function Chatlist() {
-  return <h1>Chatlist</h1>;
+  return (
+    <div className="hidden md:flex md:flex-1 shadow-[-8px_10px_10px_-5px_rgba(0,0,0,0.1)]">
+      <div className="w-full">
+        <div className="flex w-full h-[60px] items-center justify-between px-5 border-b border-neutral-200">
+          <h2 className="text-lg font-bold">Chat</h2>
+          <div className="flex justify-center gap-3">
+            <FunnelSimple
+              size={20}
+              weight="light"
+              className="hover:text-indigo-800"
+            />
+            <Gear size={20} weight="light" className="hover:text-indigo-800" />
+          </div>
+        </div>
+
+        <div>
+          <p className="text-sm px-[50px] py-5 text-center">
+            Aqui você verá menções, respostas e outras notificações.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
