f01f117a1b78a681a85b1afac756b0e7481d414d
papilon <ademirgus@gmail.com>
2023-06-08 20:25:24 -0300

    init:config firebase

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -22,7 +22,6 @@ export function Chat() {
   }
 
   return (
-    // Final 30 min
     <div className="flex-1 shadow-[-8px_10px_10px_-5px_rgba(0,0,0,0.1)] overflow-hidden">
       <div className="flex flex-col w-full h-full overflow-hidden">
         <div className="flex w-full h-[60px] items-center justify-between px-5 border-b border-neutral-200">
---
+++ src/services/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCylvGvcBUGqtKJFUwzRbVlhQrmYMk-fRU",
+
+  authDomain: "teams-clone-4882e.firebaseapp.com",
+
+  projectId: "teams-clone-4882e",
+
+  storageBucket: "teams-clone-4882e.appspot.com",
+
+  messagingSenderId: "594200949010",
+
+  appId: "1:594200949010:web:a6839cee2fc8701e0d83cb",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
