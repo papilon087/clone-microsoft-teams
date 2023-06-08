be71c0dd362be04fa2be804ef83e90c95f061a12
papilon <ademirgus@gmail.com>
2023-06-07 22:29:28 -0300

    feat:finish function chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,26 @@
-import { PencilSimple, Plus, UserPlus } from "phosphor-react";
+import { PaperPlaneRight, PencilSimple, Plus, UserPlus } from "phosphor-react";
 import comunidev_profile from "../assets/comunidev_profile.png";
+import { FormEvent, useState } from "react";
+
+interface MessagesList {
+  idMessage: string;
+  username: string;
+  message: string;
+  created_at: string;
+  avatarURL: string;
+  userId: string;
+}
 
 export function Chat() {
+  const [message, setMessage] = useState("");
+  const [messagesList, setMessagesList] = useState([]);
+
+  async function handleSubmitMessage(e: FormEvent) {
+    e.preventDefault();
+    setMessagesList([...messagesList, message]);
+    setMessage("");
+  }
+
   return (
     // Final 30 min
     <div className="flex-1 shadow-[-8px_10px_10px_-5px_rgba(0,0,0,0.1)] overflow-hidden">
@@ -25,11 +44,44 @@ export function Chat() {
             </div>
             <Plus size={20} weight="light" />
           </div>
+
           <div className="flex justify-center gap-3">
             <UserPlus size={20} weight="light" />
             <span className="text-sm">11</span>
           </div>
         </div>
+
+        <div className="flex flex-1 p-5 overflow-y-scroll">
+          <ul className="flex flex-col gap-3 w-full">
+            {messagesList ? (
+              messagesList.map((message) => {
+                return <li key={message}></li>;
+              })
+            ) : (
+              <p className="text-2xl font-bold px-[50px] py-5 text-center">
+                Nenhuma mensagem eviada até o momento
+              </p>
+            )}
+          </ul>
+        </div>
+
+        <form
+          className="flex w-full px-5 items-center justify-center gap-2 mb-6"
+          onSubmit={handleSubmitMessage}
+        >
+          <textarea
+            name="message"
+            id="message"
+            rows={1}
+            value={message}
+            className="bg-white w-full max-w-5xl p-4 resize-none sm:text-sm shadow-md border-b-2 border-indigo-700 placeholder:text-neutral-800 focus:outline-0"
+            placeholder="Digite uma mensagem aqui"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">
+            <PaperPlaneRight size={24} weight="light" />
+          </button>
+        </form>
       </div>
     </div>
   );
