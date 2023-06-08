0d64284feee85c01544e57c4bbdcbff88ff116f4
papilon <ademirgus@gmail.com>
2023-06-08 20:46:27 -0300

    feat:created page SignIn

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,17 @@
 export function SignIn() {
-  return <h1>aaa</h1>;
+  return (
+    <div className="flex flex-col h-screen items-center justify-center bg-neutral-800">
+      <div className="flex flex-col w-full max-w-md items-center justify-center gap-8">
+        <h1 className="font-bold text-3xl text-neutral-100">
+          Faça login em sua conta
+        </h1>
+        <button
+          className="flex w-full p-4 shadow-sm border rounded items-center justify-center bg-white"
+          //onClick={handleSignIn}
+        >
+          <span className="font-semibold text-lg">Entrar com o Google</span>
+        </button>
+      </div>
+    </div>
+  );
 }
